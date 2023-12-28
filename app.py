50ad82be99837a091ada9c23564c67dc5a3e025b
Leonardo Tasso <96449320+leotasso3@users.noreply.github.com>
2023-12-28 14:08:40 +0100

    add dummy coding

--- app.py
+++ app.py
@@ -7,27 +7,25 @@ import pandas as pd
 
 app=Flask(__name__)
 ## Load the model
-model=pickle.load(open('','rb'))
+model=pickle.load(open('/Users/leo/Xtream_repo/Xtream_Tasso/decision_tree.pkl','rb'))
 @app.route('/')
 def home():
-    return render_template('web_application.html')
+    return render_template('web_app_rest.html')
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
     data=request.json['data']
     print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
     output=model.predict(data)
     print(output[0])
     return jsonify(output[0])
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    data=[float(x) for x in request.form.values()]
-    final_input=model.transform(np.array(data).reshape(1,-1))
-    print(final_input)
-    output=model.predict(final_input)[0]
-    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+    data=[x for x in request.form.values()]
+    data = pd.get_dummies(data)
+    output=model.predict(data)[0]
+    return render_template("web_app_rest.html",prediction_text="The House price prediction is {}".format(output))
 
 
 
